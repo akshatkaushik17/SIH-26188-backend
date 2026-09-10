ef4f9c018058a81d7c60b759a28121c857fdb0fb
akshat <akshatkaushik100x@gmail.com>
2026-09-10 09:53:52 +0530

    Fix EasyOCR detector configuration

--- AI/processor.py
+++ AI/processor.py
@@ -30,9 +30,7 @@ def get_reader():
         reader = easyocr.Reader(
     ['en'],
     model_storage_directory=model_dir,
-    download_enabled=False,
-    gpu=False,
-    detector=False
+    gpu=False
 )
 
     return reader
